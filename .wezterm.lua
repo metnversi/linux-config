0c56c13e149888d3ff4eff4405ce87daf7e65fe4
metnversi <vinh.nt202727@sis.hust.edu.vn>
2024-08-10 09:30:20 +0700

    worked :)

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,18 +1,19 @@
-
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-config.color_scheme = 'terminal.sexy'
-config.font =
-  wezterm.font('Iosevka', { weight = 'Bold', italic = true })
+config.color_scheme = "terminal.sexy"
 
+config.font = wezterm.font("Iosevka")
+--config.font = wezterm.font("Iosevka", { weight = "Bold", italic = true })
 
-config.default_prog = { '/bin/zsh' }
+config.audible_bell = "Disabled"
+config.window_decorations = "NONE"
+config.default_prog = { "/bin/zsh" }
 config.keys = {
-  {
-    key = 'F11',
-    mods = '',
-    action = wezterm.action.ToggleFullScreen,
-  },
+	{
+		key = "F11",
+		mods = "",
+		action = wezterm.action.ToggleFullScreen,
+	},
 }
 
 return config
