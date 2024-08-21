e9014700d7acd966f841f6788e63694558a5d99a
metnversi <vinh.nt202727@sis.hust.edu.vn>
2024-08-21 14:29:33 +0700

    clipboard package

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.opt.clipboard = "unnamedplus"
+
+if vim.fn.executable("xclip") == 1 then
+  vim.opt.clipboard:append("unnamedplus")
+  vim.g.clipboard = {
+    name = "xclip",
+    copy = {
+      ["+"] = "xclip -selection clipboard",
+      ["*"] = "xclip -selection primary",
+    },
+    paste = {
+      ["+"] = "xclip -selection clipboard -o",
+      ["*"] = "xclip -selection primary -o",
+    },
+    cache_enabled = 0,
+  }
+end
