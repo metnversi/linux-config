0300be499e60b288fb990e6f13d7eab7689f59c0
metnversi <vinh.nt202727@sis.hust.edu.vn>
2024-09-21 09:42:23 +0700

    organize and fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#for other linux distro, simply change the 'apt-get'. Example 'dnf', 'yum' :>
 rm $HOME/.bashrc
 
-SCRIPT_DIR="$(cd "$(dirname "$BASH_SOUrcE[0]")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 symlinkFile() {
   filename="$SCRIPT_DIR/$1"
   destination="$HOME/$2"
 
-  mkdir -p $(dirname "$destination")
+  mkdir -p "$(dirname "$destination")"
 
-  if [ -e "$destination" ]; then
+  if [ -e "$destination" ] || [ -L "$destination" ]; then
     rm -rf "$destination"
     echo "[INFO] $destination exists and will be overwritten."
   fi
@@ -19,21 +19,16 @@ symlinkFile() {
 }
 
 deployManifest() {
-  for row in $(cat $SCRIPT_DIR/$1); do
-    filename=$(echo $row | cut -d \| -f 1)
-    operation=$(echo $row | cut -d \| -f 2)
-    destination=$(echo $row | cut -d \| -f 3)
-
+  while IFS='|' read -r filename operation destination; do
     case $operation in
     symlink | override)
-      symlinkFile $filename $destination
+      symlinkFile "$filename" "$destination"
       ;;
-
     *)
       echo "[WARNING] Unknown operation $operation. Skipping..."
       ;;
     esac
-  done
+  done <"$SCRIPT_DIR/$1"
 }
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
